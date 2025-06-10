95cbaba420723fef8662c8da0d8368e22c9dc208
yichihwu <edward2001921@gmail.com>
2025-06-10 14:42:47 +0800

    113618002 final answer

--- nn_predict.py
+++ nn_predict.py
@@ -8,8 +8,11 @@ def relu(x):
 
 def softmax(x):
     # TODO: Implement the SoftMax function
-    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
-    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+  stable_x = x - np.max(x, axis=-1, keepdims=True)
+  e_x = np.exp(stable_x)
+  
+  # 沿著最後一個維度計算總和
+  return e_x / np.sum(e_x, axis=-1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
